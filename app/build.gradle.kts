70c6898b594862b1a597212906be32dc2f30e115
chamudith bandara <chamudithbuddima@gmail.com>
2026-06-18 13:19:41 +0530

    Add Lombok dependency for improved code generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Source: https://mvnrepository.com/artifact/org.projectlombok/lombok
+    implementation("org.projectlombok:lombok:1.18.46")
 }
