c2239b68e4d75e6e303dde6d3407542673422e3d
chamudith bandara <chamudithbuddima@gmail.com>
2026-06-18 13:18:06 +0530

    Add ProductDTO class with fields and accessor methods

--- app/src/main/java/com/quickmart/android/dto/ProductDTO.java
+++ app/src/main/java/com/quickmart/android/dto/ProductDTO.java
@@ -1,4 +1,35 @@
 package com.quickmart.android.dto;
 
-public class ProductDto {
+public class ProductDTO {
+
+    private String name;
+    private Double price;
+    private Integer qty;
+
+    public String getName() {
+        return name;
+    }
+
+    public ProductDTO setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public ProductDTO setPrice(Double price) {
+        this.price = price;
+        return this;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public ProductDTO setQty(Integer qty) {
+        this.qty = qty;
+        return this;
+    }
 }
